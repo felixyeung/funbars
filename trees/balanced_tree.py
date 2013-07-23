e500b4b57846dab7d2a5c83653948cbbd5e60d75
Felix Yeung <felix.j.yeung@gmail.com>
2013-07-22 21:33:56 -0700

    check for tree balance

---
+++ trees/balanced_tree.py
@@ -0,0 +1,58 @@
+class MyNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def addLeft(self, node):
+        self.left = node
+
+    def addRight(self, node):
+        self.right = node
+
+    def isBalanced(self):
+        print self.countDepth(self.left) 
+        print self.countDepth(self.right)
+        return self.countDepth(self.left) == self.countDepth(self.right)
+
+    def countDepth(self, node):
+        if node == None:
+            return 0;
+        else:
+            return 1 + max(self.countDepth(node.left), self.countDepth(node.right)) 
+
+root = MyNode('a')
+b = MyNode('b')
+c = MyNode('c')
+d = MyNode('d')
+e = MyNode('e')
+f = MyNode('f')
+g = MyNode('g')
+h = MyNode('h')
+i = MyNode('i')
+j = MyNode('j')
+k = MyNode('k')
+l = MyNode('l')
+m = MyNode('m')
+
+root.addLeft(b)
+root.addRight(c)
+
+b.addLeft(d)
+b.addRight(g)
+
+d.addLeft(e)
+d.addRight(f)
+
+e.addLeft(h)
+
+c.addLeft(i)
+c.addRight(j)
+
+i.addLeft(k)
+
+j.addLeft(l)
+
+l.addLeft(m)
+
+root.isBalanced()
